eac1334ac1cac57ecd928e6813de9c380d9f7beb
Jonathan Fekadu <jonathanfekadu1@gmail.com>
2023-10-19 22:37:38 +0300

    finished the switch effect of the front page

--- js/landing.js
+++ js/landing.js
@@ -1,53 +1,32 @@
 const slider = document.querySelector(".slider");
-const sliderImage = slider.querySelector("img");
+const sliderImages = slider.querySelectorAll("img");
 const imageTracker = document.querySelectorAll(".img-no-teller li");
 
-let id = 1;
-sliderImage.src = `Images/SV1.png`;
+let id = 0;
 
-imageTracker.forEach((track) => {
-  if (track.id == id) {
-    track.style.opacity = "0.3";
-  }
-});
-
-const frontPage = document.querySelector("#front_page");
-const buttons = frontPage.querySelectorAll("button");
-buttons.forEach((btn) => {
-  if (id < 1) {
-    btn.style.opacity = "0.1";
-    btn.disabled = true;
-  }
-  btn.onclick = () => {
-    sliderImage.className = "";
-    if (btn.innerText === "right") {
-      id++;
-      if (id > 3) {
-        id = 1;
+setInterval(() => {
+  id++;
+  if (id === 3) {
+    sliderImages.forEach((img) => {
+      if (img.hasAttribute("slider-img")) {
+        img.style.opacity = "0";
       }
-      sliderImage.src = `Images/SV${id}.png`;
-      imageTracker.forEach((track) => {
-        track.style.opacity = "1";
-        if (track.id == id) {
-          track.style.opacity = "0.3";
-        }
-      });
-      sliderImage.className = "active";
-    } else if (btn.innerText === "left") {
-      id--;
-      if (id < 1) {
-        sliderImage.src = `Images/SV1.png`;
-        id = 1;
-      } else {
-        sliderImage.src = `Images/SV${id}.png`;
-        imageTracker.forEach((track) => {
-          track.style.opacity = "1";
-          if (track.id == id) {
-            track.style.opacity = "0.3";
-          }
-        });
-        sliderImage.className = "active";
+    });
+    id = 0;
+  }
+  sliderImages.forEach((img) => {
+    if (img.hasAttribute("slider-img")) {
+      if (img.id == id) {
+        img.style.opacity = "1";
       }
     }
-  };
-});
+  });
+  imageTracker.forEach((track) => {
+    track.style.opacity = "0.6";
+    track.style.transform = "scale(1)";
+    if (track.id == id) {
+      track.style.opacity = "1";
+      track.style.transform = "scale(1.2)";
+    }
+  });
+}, 5000);
