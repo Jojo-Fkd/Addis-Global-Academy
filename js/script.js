6283b42b1a81fe8e4792c83b2fa728c2d48c643f
Jonathan Fekadu <jonathanfekadu1@gmail.com>
2023-10-26 12:20:34 +0300

    finished the responsive design of every page except news and events

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* FRONT PAGE SLIDE EFFECT */
+
 const slider = document.querySelector(".slider");
 const sliderImages = slider.querySelectorAll("img");
 const imageTracker = document.querySelectorAll(".img-no-teller li");
@@ -38,3 +40,29 @@ setInterval(() => {
     }
   });
 }, 5000);
+
+/* FOOTER CARET FUNCTION */
+
+const caretIcons = document.querySelectorAll(".caret_down_container");
+
+caretIcons.forEach((caret) => {
+  let caretInfo = caret.parentElement.parentElement;
+  caret.onclick = () => {
+    caret.classList.toggle("active");
+    caretInfo.classList.toggle("active");
+  };
+});
+
+/* FAQ SECTION */
+
+const faqIcons = document.querySelectorAll(
+  ".faq-body section article .answer_toggle"
+);
+
+faqIcons.forEach((icon) => {
+  icon.onclick = () => {
+    const faq = icon.parentElement.parentElement;
+    icon.classList.toggle("active");
+    faq.classList.toggle("active");
+  };
+});
